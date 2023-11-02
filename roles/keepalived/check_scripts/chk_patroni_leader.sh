eeed202cc15b0dbd5911586d3b4825cc24f6eef7
Alexandr Tokarev <alexandr.tokarev@tantorlabs.ru>
2023-11-02 17:09:39 +0400

    add patroni scripts and config

---
+++ roles/keepalived/check_scripts/chk_patroni_leader.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NODENAME=$(cat /etc/patroni/{{ inventory_hostname }}.yml | grep -E "^name:" | cut -d: -f2 | tr -d '[:blank:]')
+
+if [[ -z "$NODENAME" ]]; then
+   echo "Nodename is blank!"
+   exit 1
+fi
+
+PATRONICTL_OUT=$(patronictl -c /etc/patroni/{{ inventory_hostname }}.yml list --format json)
+
+if [[ -z "$PATRONICTL_OUT" ]]; then
+   echo "No patronictl output!"
+   exit 1
+fi     
+
+LEADER=$(echo $PATRONICTL_OUT | jq --raw-output ".[] | select ((.Role == \"Leader\") and (.State == \"running\")) | .Member")
+
+if [[ "$NODENAME" == "$LEADER" ]]; then
+    echo "Is leader!"
+    exit 0
+else
+    echo "Is not leader!"
+    exit 1
+fi
