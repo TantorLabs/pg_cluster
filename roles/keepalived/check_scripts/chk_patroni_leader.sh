56c3e5f551ed35b2337d38fbf9da7b3424e85065
Maxim Bagirov <maxim.bagirov@tantorlabs.ru>
2025-06-18 16:45:51 +0300

    Changes that have been made: * replace sh certificate generation procedure to the ansible-way; * add scaling functionality for etcd nodes; * replace default DB versions; * minor changes in var names;

--- roles/keepalived/check_scripts/chk_patroni_leader.sh
+++ roles/keepalived/check_scripts/chk_patroni_leader.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-NODENAME=$(cat /opt/tantor/etc/patroni/{{ inventory_hostname }}.yml | grep -E "^name:" | cut -d: -f2 | tr -d '[:blank:]')
+NODENAME=$(cat {{ patroni_config_dir }}/{{ inventory_hostname }}.yml | grep -E "^name:" | cut -d: -f2 | tr -d '[:blank:]')
 
 if [[ -z "$NODENAME" ]]; then
    echo "Nodename is blank!"
    exit 1
 fi
 
-PATRONICTL_OUT=$(/opt/tantor/usr/bin/patronictl -c /opt/tantor/etc/patroni/{{ inventory_hostname }}.yml list --format json)
+PATRONICTL_OUT=$(/opt/tantor/usr/bin/patronictl -c {{ patroni_config_dir }}/{{ inventory_hostname }}.yml list --format json)
 
 if [[ -z "$PATRONICTL_OUT" ]]; then
    echo "No patronictl output!"
